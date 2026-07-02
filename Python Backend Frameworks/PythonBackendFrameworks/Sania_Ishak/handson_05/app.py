2d25c90213132c588cb5ad4b795bbd11779c6436
SaniaIshak <saniaishak007@gmail.com>
2026-07-02 09:40:42 +0530

    Add files via upload

---
+++ Python Backend Frameworks/PythonBackendFrameworks/Sania_Ishak/handson_05/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///course.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from courses.routes import courses_bp
+    app.register_blueprint(courses_bp)
+
+    return app
